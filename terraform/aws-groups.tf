f067626006ba98f5f3808e6b651ac296de7cdbf0
Chelsey Beck <cbeck@mozilla.com>
2024-02-01 21:43:04 -0800

    cleaning up files

--- terraform/aws-groups.tf
+++ terraform/aws-groups.tf
@@ -5,38 +5,3 @@ module "iam_read_only_group" {
   group_name = "read-only-group"
   policy_arn = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
 }
-
-// Create project admin group - this group is dependent on tagging resources
-# module "iam_project_admin_group" {
-#   source = "./modules/groups"
-
-#   group_name = "project-admin-group"
-#   policy_arn = [ # here we can pass a list of policies that are aws managed or customer managed
-#     "arn:aws:iam::aws:policy/SomeAWSPolicy",
-#     module.iam_policies.policy_arns["ManageAccessKeys"]
-#   ]
-# }
-
-// Create mentor group - need to discuss these permissions
-# module "iam_ops_mentor_group" {
-#   source = "./modules/groups"
-
-#   group_name = "ops-mentor-group"
-#   policy_arn = [ # here we can pass a list of policies that are aws managed or customer managed
-#     "arn:aws:iam::aws:policy/AdministratorAccess",
-#     "arn:aws:iam::035866691871:policy/ManageAccessKeys",
-#   ]
-# }
-
-// Create IAM management group -- 
-# module "iam_services_supervisor_group" {
-#   source = "./modules/groups"
-
-#   group_name = "iam-services-supervisor-group"
-#   policy_arn = [ # here we can pass a list of policies that are aws managed or customer managed
-#     "arn:aws:iam::aws:policy/AdministratorAccess",
-#     "arn:aws:iam::035866691871:policy/ManageAccessKeys",
-#   ]
-# }
-
-// Cost management group = "Cost-Management"
--- terraform/aws-roles.tf
+++
@@ -1,6 +0,0 @@
-# module "iam_role_example" {
-#   source                = "./modules/roles"
-#   role_name             = "example_role"
-#   assume_role_principal = "ec2.amazonaws.com"
-#   policy_arn            = "arn:aws:iam::aws:policy/ExamplePolicy"
-# }
