5343460301160750124105cf9484a2082de38ad0
Chelsey Beck <cbeck@mozilla.com>
2024-02-12 22:05:13 -0800

    testing permissions with updated path

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "hfla-ops-terraform-state"
-    key            = "hfla-ops-terraform-state/devops-security/terraform.tfstate"
+    key            = "s3://hfla-ops-terraform-state/devops-security/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "hfla_ops_terraform_table"
     encrypt        = true
