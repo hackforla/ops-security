79cd02759d4fbaf31699467b7f9a81dec8a9b544
Chelsey Beck <cbeck@mozilla.com>
2024-02-19 15:30:22 -0800

    adding new users

--- terraform/aws-users.tf
+++ terraform/aws-users.tf
@@ -1,11 +1,80 @@
 // Create user and assign to group(s)
-module "iam_user_gwenstacy" {
+module "iam_user_JimmyJuarez10" {
   source = "./modules/aws-users"
 
-  user_name = "gwenstacy"
+  user_name = "JimmyJuarez10"
   user_tags = {
-    "Environment" = "Development"
-    "Project"     = "spiderverse"
+    "Project"     = "civic-tech-jobs"
+  }
+  user_groups = ["read-only-group"]
+}
+
+module "iam_user_brittanyms" {
+  source = "./modules/aws-users"
+
+  user_name = "brittanyms"
+  user_tags = {
+    "Project"     = "devops-security"
+  }
+  user_groups = ["read-only-group"]
+}
+
+module "iam_user_freaky4wrld" {
+  source = "./modules/aws-users"
+
+  user_name = "freaky4wrld"
+  user_tags = {
+    "Project"     = "devops-security"
+  }
+  user_groups = ["read-only-group"]
+}
+
+module "iam_user_shikha0428" {
+  source = "./modules/aws-users"
+
+  user_name = "shikha0428"
+  user_tags = {
+    "Project"     = "devops-security"
+  }
+  user_groups = ["read-only-group"]
+}
+
+module "iam_user_shinjonathan" {
+  source = "./modules/aws-users"
+
+  user_name = "shinjonathan"
+  user_tags = {
+    "Project"     = "devops-security"
+  }
+  user_groups = ["read-only-group"]
+}
+
+module "iam_user_samuelusc" {
+  source = "./modules/aws-users"
+
+  user_name = "samuelusc"
+  user_tags = {
+    "Project"     = "devops-security"
+  }
+  user_groups = ["read-only-group"]
+}
+
+module "iam_user_abbyz123" {
+  source = "./modules/aws-users"
+
+  user_name = "abbyz123"
+  user_tags = {
+    "Project"     = "devops-security"
+  }
+  user_groups = ["read-only-group"]
+}
+
+module "iam_user_awlFCCamp" {
+  source = "./modules/aws-users"
+
+  user_name = "awlFCCamp"
+  user_tags = {
+    "Project"     = "devops-security"
   }
   user_groups = ["read-only-group"]
 }
