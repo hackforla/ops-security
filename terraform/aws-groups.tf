343ddbd9ced06da687ac1d1dd103a82118542312
Chelsey Beck <cbeck@mozilla.com>
2024-02-20 11:41:36 -0800

    updating policy arn to use a map and adding the admin group

--- terraform/aws-groups.tf
+++ terraform/aws-groups.tf
@@ -3,5 +3,19 @@ module "iam_read_only_group" {
   source = "./modules/aws-groups"
 
   group_name = "read-only-group"
-  policy_arn = ["arn:aws:iam::aws:policy/ReadOnlyAccess", "arn:aws:iam::aws:policy/IAMUserChangePassword"]
+  policy_arn = {
+    "ReadOnlyAccess"        = "arn:aws:iam::aws:policy/ReadOnlyAccess",
+    "IAMUserChangePassword" = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+  }
 }
+
+// Create iam services admin group
+module "iam_services_admin_group" {
+  source = "./modules/aws-groups"
+
+  group_name = "iam-services-admin-group"
+  policy_arn = {
+    "IAMServicesAdmin" = module.aws_custom_policies.policy_arns["IAMServicesAdmin"]
+  }
+}
+
