52e1a969466985a78b8833b0b68b0452fd309204
luk020 <lsousadev@gmail.com>
2024-10-30 18:45:52 -0700

    fix: ran tf fmt

--- terraform/aws-gha-oidc-providers.tf
+++ terraform/aws-gha-oidc-providers.tf
@@ -1,10 +1,10 @@
 module "iam_oidc_gha_incubator" {
-  source             = "./modules/aws-gha-oidc-providers"
+  source = "./modules/aws-gha-oidc-providers"
 
-  role_name          = "gha-incubator"
-  use_wildcard       = true
-  github_branch      = "refs/heads/*" # allows any branch
-  github_repo        = "hackforla/incubator"
+  role_name     = "gha-incubator"
+  use_wildcard  = true
+  github_branch = "refs/heads/*" # allows any branch
+  github_repo   = "hackforla/incubator"
 
   policy_arns = [
     "arn:aws:iam::aws:policy/AdministratorAccess"
